1087f37c396fb4bfb2a8c6a3def12c0d4b3f3ef5
Rafael Sutil da Costa <rafasutildacosta@gmail.com>
2025-07-23 10:34:39 -0300

    Fix responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,40 @@
+"use client"
+
+import navItems from "../../public/data/navItems.json";
+import { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+
 const Navbar = () => {
+    const [dropdown, setDropdown] = useState(true);
+
     return(
         <nav className="flex bg-[#015486] h-12 w-full text-white px-12 place-items-center content-center">
-            <div className="flex w-1/2 justify-start ">
+            <div className="flex w-1/2 justify-start">
                 <img className="w-16" src="./pHbot-semfundo.png" alt="teste" />
             </div>
-            <div className="flex w-1/2 justify-end">
-                <button className="px-2 hover:cursor-pointer">
-                    Quem somos
-                </button>
-                <button className="px-2 hover:cursor-pointer">
-                    Soluções
-                </button>
-                <button className="px-2 hover:cursor-pointer">
-                    Contato
-                </button>
+            <div className="sm:flex hidden w-1/2 justify-end">
+                {Object.values(navItems).map((item, index) => (
+                    <a key={index} href={item.href} className="px-2 hover:cursor-pointer">
+                        {item.name}
+                    </a>
+                ))}
             </div>
+            <div className="sm:hidden w-1/2 justify-items-end" onClick={() => setDropdown(!dropdown)}>
+             <AiOutlineMenu className='lg:hidden flex text-white hover:cursor-pointer'/>   
+            </div>
+            {dropdown ? (
+                <div className="sm:hidden absolute right-10 top-10 justify bg-white drop-shadow-sm h-fit rounded-lg text-black">
+                    {Object.values(navItems).map((item, index) => (
+                    <a key={index} href={item.href} className="flex w-full rounded-sm hover:bg-[#015486] hover:text-white p-2 hover:cursor-pointer">
+                        {item.name}
+                    </a>
+                ))}
+                </div>
+            ) : (
+                <div className="hidden">
+                    
+                </div>
+            )}
         </nav>
     )
 }
